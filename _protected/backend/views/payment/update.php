dfeee204f7238dd72ecc39a54a0359e915f1a32d
bswify <34600468+bswify@users.noreply.github.com>
2018-01-18 01:07:10 +0700

    (bee) edit payment

--- _protected/backend/views/payment/update.php
+++ _protected/backend/views/payment/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Payment */
 
-$this->title = 'แก้ไขข้อมูลประเภทการชำระเงิน: {nameAttribute}';
+$this->title = 'แก้ไขข้อมูลประเภทการชำระเงิน: '.$model->IDPaymant;
 $this->params['breadcrumbs'][] = ['label' => 'ข้อมูลประเภทการชำระเงิน', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->IDPaymant, 'url' => ['view', 'id' => $model->IDPaymant]];
 $this->params['breadcrumbs'][] = 'Update';
